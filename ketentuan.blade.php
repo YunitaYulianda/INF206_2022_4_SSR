e1df4b1c655b447085f1d8791417b3d37ea8c6a0
YunitaYulianda <81976317+YunitaYulianda@users.noreply.github.com>
2022-04-25 11:10:13 +0700

    Update ketentuan.blade.php

--- ketentuan.blade.php
+++ ketentuan.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
   <head>
     <!-- ============= META ============ -->
     <meta charset="UTF-8" />
@@ -26,6 +27,7 @@
     <!-- ============= CSS ============= -->
     <link rel="stylesheet" href="css/style.css" />
   </head>
+  
   <body class="b-ketentuan">
     <!-- ========== KETENTUAN ========== -->
     <section class="ketentuan">
