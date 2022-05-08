386c4d7e3792692db07430a5a8bd045424603340
YunitaYulianda <81976317+YunitaYulianda@users.noreply.github.com>
2022-05-08 23:55:48 +0700

    Update contact-person.blade.php

--- contact-person.blade.php
+++ contact-person.blade.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
+
 <html lang="en">
+
   <head>
     <!-- ============= META ============ -->
     <meta charset="UTF-8" />
@@ -52,5 +54,6 @@
       integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
       crossorigin="anonymous"
     ></script>
+    
   </body>
 </html>
